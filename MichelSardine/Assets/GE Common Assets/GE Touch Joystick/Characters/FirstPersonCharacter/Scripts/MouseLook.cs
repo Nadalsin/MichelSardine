25298f1cdc2c8f9859837d07434419b751b5ea54
steve nadalin <steve nadalin>
2017-04-18 16:14:39 +0200

    add assets

--- MichelSardine/Assets/GE Common Assets/GE Touch Joystick/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
+++ MichelSardine/Assets/GE Common Assets/GE Touch Joystick/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
@@ -1,57 +1,73 @@
 using System;
 using UnityEngine;
-using UnitySampleAssets.CrossPlatformInput;
+using UnityStandardAssets.CrossPlatformInput;
 
-namespace UnitySampleAssets.Characters.FirstPerson
+namespace UnityStandardAssets.Characters.FirstPerson
 {
-	[Serializable]
-	public class MouseLook
-	{
-		public float XSensitivity = 15f;
-		public float YSensitivity = 15f;
-		public float MinimumX = -360F;
-		public float MaximumX = 360F;
-		public float MinimumY = -90F;
-		public float MaximumY = 90F;
-
-		private float xvel = 0f;
-		private float yvel = 0f;
-		public bool smooth;
-		public float smoothtime;
+    [Serializable]
+    public class MouseLook
+    {
+        public float XSensitivity = 2f;
+        public float YSensitivity = 2f;
+        public bool clampVerticalRotation = true;
+        public float MinimumX = -90F;
+        public float MaximumX = 90F;
+        public bool smooth;
+        public float smoothTime = 5f;
 
 
-		public Vector2 UnClamped(float x, float y)
-		{
-			Vector2 value;
-			value.x = y + CrossPlatformInputManager.GetAxis("Mouse X")*XSensitivity;
-			value.y = x + CrossPlatformInputManager.GetAxis("Mouse Y")*YSensitivity;
-
-			if (smooth)
-			{
-				value.x = Mathf.SmoothDamp(y, value.x, ref xvel, smoothtime);
-				value.y = Mathf.SmoothDamp(x, value.y, ref yvel, smoothtime);
-			}
-
-			return value;
-		}
+        private Quaternion m_CharacterTargetRot;
+        private Quaternion m_CameraTargetRot;
 
 
-		public Vector2 Clamped(float x, float y)
-		{
-			Vector2 value;
-			value.x = y + CrossPlatformInputManager.GetAxis("Mouse X")*XSensitivity;
-			value.y = x + CrossPlatformInputManager.GetAxis("Mouse Y")*YSensitivity;
+        public void Init(Transform character, Transform camera)
+        {
+            m_CharacterTargetRot = character.localRotation;
+            m_CameraTargetRot = camera.localRotation;
+        }
 
-			value.x = Mathf.Clamp(value.x, MinimumX, MaximumX);
-			value.y = Mathf.Clamp(value.y, MinimumY, MaximumY);
 
-			if (smooth)
-			{
-				value.x = Mathf.SmoothDamp(y, value.x, ref xvel, smoothtime);
-				value.y = Mathf.SmoothDamp(x, value.y, ref yvel, smoothtime);
-			}
+        public void LookRotation(Transform character, Transform camera)
+        {
+            float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * XSensitivity;
+            float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * YSensitivity;
 
-			return value;
-		}
-	}
+            m_CharacterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
+            m_CameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
+
+            if(clampVerticalRotation)
+                m_CameraTargetRot = ClampRotationAroundXAxis (m_CameraTargetRot);
+
+            if(smooth)
+            {
+                character.localRotation = Quaternion.Slerp (character.localRotation, m_CharacterTargetRot,
+                    smoothTime * Time.deltaTime);
+                camera.localRotation = Quaternion.Slerp (camera.localRotation, m_CameraTargetRot,
+                    smoothTime * Time.deltaTime);
+            }
+            else
+            {
+                character.localRotation = m_CharacterTargetRot;
+                camera.localRotation = m_CameraTargetRot;
+            }
+        }
+
+
+        Quaternion ClampRotationAroundXAxis(Quaternion q)
+        {
+            q.x /= q.w;
+            q.y /= q.w;
+            q.z /= q.w;
+            q.w = 1.0f;
+
+            float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan (q.x);
+
+            angleX = Mathf.Clamp (angleX, MinimumX, MaximumX);
+
+            q.x = Mathf.Tan (0.5f * Mathf.Deg2Rad * angleX);
+
+            return q;
+        }
+
+    }
 }
